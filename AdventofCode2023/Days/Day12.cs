0c009d33af5ed42a1b6a69b1893a7bfa987dc453
Jacob Graybill <jgraybill@gatech.edu>
2023-12-14 11:50:17 -0500

    Day 13 Part 2

--- AdventofCode2023/Days/Day12.cs
+++ AdventofCode2023/Days/Day12.cs
@@ -12,7 +12,7 @@ namespace AdventofCode2023.Days
     {
         private int DayNum = 12;
         private string InputFile;
-        private bool UseTestingFile = false;
+        private bool UseTestingFile = true;
 
         public Day12()
         {
@@ -38,10 +38,10 @@ namespace AdventofCode2023.Days
                     List<int> variableIndexes = new List<int>();
                     char placeHolder = '?';
 
-                    springRow = string.Concat(Enumerable.Repeat(new string(springRow) + "?", 5)).TrimEnd('?').ToCharArray();
-                    sequenceConstraints = string.Join(',', Enumerable.Repeat(lineParts[1], 5)).Split(',').Select(x => Convert.ToInt32(x)).ToArray();
-
-
+                    //springRow = ReplaceExternalSequences(springRow, sequenceConstraints);
+                    //springRow = RemoveExistingSequenceBreakers(springRow, sequenceConstraints);
+                    //springRow = RemoveInsufficientUnknowns(springRow, sequenceConstraints);
+                    //springRow = CheckConditionsMet(springRow, sequenceConstraints);
 
                     for (int i = 0; i < springRow.Length; i++)
                     {
@@ -50,8 +50,25 @@ namespace AdventofCode2023.Days
                             variableIndexes.Add(i);
                         }
                     }
-                    List<string> potentialCombinations = GenerateCombinations(variableIndexes.Count, charOpts,
-                        charOpts.Select(x => x.ToString()).ToList(), 1);
+
+                    List<string> potentialCombinations = new List<string>();
+
+                    if (variableIndexes.Count == 0)
+                    {
+                        solutionCount += 1;
+                        continue;
+                    }
+                    else if(variableIndexes.Count == 1)
+                    {
+                        potentialCombinations.Add("#");
+                        potentialCombinations.Add(".");
+                    }
+                    else
+                    {
+                        potentialCombinations = GenerateCombinations(variableIndexes.Count, charOpts,
+                            charOpts.Select(x => x.ToString()).ToList(), 1);
+                    }
+  
 
                     int validCount = 0;
 
@@ -82,7 +99,7 @@ namespace AdventofCode2023.Days
                         }
                         
                     }
-                    //Console.WriteLine(validCount);
+                    Console.WriteLine(validCount);
 
                     solutionCount += validCount;
 
@@ -165,6 +182,333 @@ namespace AdventofCode2023.Days
             return solutionCount;
         }
 
+        private char[] RemoveInsufficientUnknowns(char[] springRow, int[] sequenceConstraints)
+        {
+            List<int> sequence = new List<int>();
+            bool replace = false;
+
+            for (int i = 0; i < springRow.Length; i++)
+            {
+                if (springRow[i] == '#')
+                {
+                    break;
+                }
+                else if (springRow[i] == '?')
+                {
+                    sequence.Add(i);
+                }
+                else if(springRow[i] != '?' && sequence.Count > 0)
+                {
+                    if(sequence.Count < sequenceConstraints.First())
+                    {
+                        replace = true;
+                    }
+                    break;
+                }
+                
+            }
+
+            if (replace)
+            {
+                for(int i = 0; i < sequence.Count; i++)
+                {
+                    springRow[sequence[i]] = '.';
+                }
+            }
+
+            sequence.Clear();
+            replace = false;
+
+            for (int i = springRow.Length - 1; i >= 0; i--)
+            {
+                if (springRow[i] == '#')
+                {
+                    break;
+                }
+                else if (springRow[i] == '?')
+                {
+                    sequence.Add(i);
+                }
+                else if (springRow[i] != '?' && sequence.Count > 0)
+                {
+                    if (sequence.Count < sequenceConstraints.Last())
+                    {
+                        replace = true;
+                    }
+                    break;
+                }
+                else if (springRow[i] == '#')
+                {
+                    break;
+                }
+            }
+
+            if (replace)
+            {
+                for (int i = 0; i < sequence.Count; i++)
+                {
+                    springRow[sequence[i]] = '.';
+                }
+            }
+
+            return springRow;
+        }
+
+        private char[] CheckConditionsMet(char[] springRow, int[] sequenceConstraints)
+        {
+            List<int> sequence = new List<int>();
+            int constraintIndex = 0;
+            bool[] constraintsMet = new bool[sequenceConstraints.Length];
+
+            for(int i = 0; i < constraintsMet.Length; i++)
+            {
+                constraintsMet[i] = false;
+            }
+
+            for (int i = 0; i < springRow.Length; i++)
+            {
+                if (springRow[i] == '#')
+                {
+                    sequence.Add(i);
+                }
+                else if (springRow[i] != '#' && sequence.Count > 0)
+                {
+                    if (sequence.Count == sequenceConstraints[constraintIndex])
+                    {
+                        constraintsMet[constraintIndex] = true;
+                        constraintIndex++;
+                        sequence.Clear();
+                    }
+                }
+                else
+                {
+                    sequence.Clear();
+                }
+            }
+
+
+            if(constraintsMet.All(x => x))
+            {
+                return new string(springRow).Replace('?', '.').ToCharArray();
+            }
+
+            return springRow;
+
+        }
+
+
+        private char[] RemoveExistingSequenceBreakers(char[] springRow, int[] sequenceConstraints)
+        {
+            List<int> sequence = new List<int>();
+            List<int> qIndicies = new List<int>();
+            for(int i = 0; i < springRow.Length; i++)
+            {
+                if (springRow[i] == '#')
+                {
+                    sequence.Add(i);
+                }
+                else if(springRow[i] != '#' && sequence.Count > 0)
+                {
+                    if(sequence.Count == sequenceConstraints.First())
+                    {
+                        if(qIndicies.Count > sequenceConstraints.First())
+                        {
+                            break;
+                        }
+
+                        if(sequence.First() > 0)
+                        {
+                            springRow[sequence.First() - 1] = '.';
+                        }
+                        if (sequence.Last() < springRow.Length - 1)
+                        {
+                            springRow[sequence.Last() + 1] = '.';
+                        }
+                    }
+
+                    
+                    //currentConstraintIndex++;
+
+                    //if(currentConstraintIndex == sequenceConstraints.Length)
+                    //{
+                    break;
+                    //}
+                }
+                else if (springRow[i] == '?')
+                {
+                    qIndicies.Add(i);
+                }
+            }
+
+
+            sequence.Clear();
+            qIndicies.Clear();
+
+            for (int i = springRow.Length - 1; i >= 0; i--)
+            {
+                if (springRow[i] == '#')
+                {
+                    sequence.Add(i);
+                }
+                else if (springRow[i] != '#' && sequence.Count > 0)
+                {
+                    if (sequence.Count == sequenceConstraints.Last())
+                    {
+                        if (qIndicies.Count > sequenceConstraints.Last())
+                        {
+                            break;
+                        }
+
+                        if (sequence.Last() > 0)
+                        {
+                            springRow[sequence.Last() - 1] = '.';
+                        }
+                        if (sequence.First() < springRow.Length - 1)
+                        {
+                            springRow[sequence.First() + 1] = '.';
+                        }
+                    }
+
+                    sequence.Clear();
+                    //currentConstraintIndex++;
+
+                    //if(currentConstraintIndex == sequenceConstraints.Length)
+                    //{
+                    break;
+                    //}
+                }
+                else if (springRow[i] == '?')
+                {
+                    qIndicies.Add(i);
+                }
+            }
+
+            return springRow;
+
+        }
+
+
+        private char[] ReplaceExternalSequences(char[] springRow, int[] sequenceConstraints)
+        {
+            int sequenceNeeded = sequenceConstraints.Last();
+            List<int> sequence = new List<int>();
+
+            for(int i = springRow.Length - 1; i >= 0; i--)
+            {
+                if (springRow[i] == '#')
+                {
+                    break;
+                }
+                else if (springRow[i] == '?')
+                {
+                    sequence.Add(i);
+                }
+                else if(sequence.Count > 0)
+                {
+                    if(sequence.Count < sequenceNeeded)
+                    {
+                        foreach(var item in sequence)
+                        {
+                            springRow[item] = '.';
+                        }
+                    }
+                }
+            }
+            sequence.Clear();
+
+            if(sequenceNeeded > 1)
+            {
+                for (int i = springRow.Length - 1; i >= 0; i--)
+                {
+                    if (new char[] { '?', '#' }.Contains(springRow[i]))
+                    {
+                        sequence.Add(i);
+
+                        if (sequence.Count == sequenceNeeded)
+                        {
+                            // Match could contain next char, don't set it
+                            if (i > 0 && new char[] { '?', '#' }.Contains(springRow[i - 1]))
+                            {
+                                break;
+                            }
+
+                            foreach (var item in sequence.Where(x => x != '#'))
+                            {
+                                springRow[item] = '#';
+                            }
+                            break;
+                        }
+                    }
+                    else if (sequence.Count > 0)
+                    {
+                        break;
+                    }
+                }
+
+                sequence.Clear();
+            }
+
+            sequenceNeeded = sequenceConstraints.First();
+
+            for (int i = 0; i < springRow.Length; i++)
+            {
+                if (springRow[i] == '#')
+                {
+                    break;
+                }
+                if (springRow[i] == '?')
+                {
+                    sequence.Add(i);
+                }
+                else if (sequence.Count > 0)
+                {
+                    if (sequence.Count < sequenceNeeded)
+                    {
+                        foreach (var item in sequence)
+                        {
+                            springRow[item] = '.';
+                        }
+                    }
+                }
+            }
+            sequence.Clear();
+
+            if(sequenceNeeded > 1)
+            {
+                for (int i = 0; i < springRow.Length; i++)
+                {
+                    if (new char[] { '?', '#' }.Contains(springRow[i]))
+                    {
+                        sequence.Add(i);
+
+                        if (sequence.Count == sequenceNeeded)
+                        {
+                            // Match could contain next char, don't set it
+                            if (i < springRow.Length - 1
+                                && new char[] { '?', '#' }.Contains(springRow[i + 1]))
+                            {
+                                break;
+                            }
+
+                            foreach (var item in sequence.Where(x => x != '#'))
+                            {
+                                springRow[item] = '#';
+                            }
+                            break;
+                        }
+                    }
+                    else if (sequence.Count > 0)
+                    {
+                        break;
+                    }
+
+                }
+            }
+            
+
+            return springRow;
+
+        }
 
         private bool CheckPotentialCombination(char[] springRow, int[] sequenceConstraints)
         {
@@ -188,8 +532,11 @@ namespace AdventofCode2023.Days
             return match.Success && match.Value.Count(x => x == '#') == springRow.Count(x => x == '#');
         }
 
-        private List<string> GenerateCombinations(int count, List<char> chars, List<string> combinations, int layer)
+        private List<string> GenerateCombinations(int count,
+            List<char> chars,
+            List<string> combinations, int layer)
         {
+
             List<string> remove = new List<string>();
             List<string> add = new List<string>();
 
--- AdventofCode2023/Days/Day13.cs
+++ AdventofCode2023/Days/Day13.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdventofCode2023.Days
+﻿namespace AdventofCode2023.Days
 {
     public class Day13 : IDay
     {
@@ -20,32 +13,333 @@ namespace AdventofCode2023.Days
 
         public int Problem1()
         {
-            string? line;
-            using (var fileStream = File.OpenRead(InputFile))
-            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true))
-            {
-                while ((line = streamReader.ReadLine()) != null)
-                {
+            string[] lines = File.ReadAllLines(InputFile);
+            List<List<string>> patterns = new List<List<string>>();
+            List<string> pattern = new List<string>();
 
+            for(int lineNum = 0; lineNum < lines.Length; lineNum++)
+            {
+                if (String.IsNullOrEmpty(lines[lineNum]))
+                {
+                    patterns.Add(new List<string>(pattern));
+                    pattern.Clear();
+                    continue;
                 }
+
+                pattern.Add(lines[lineNum]);
             }
 
-            return 0;
+            if(pattern.Count > 0)
+            {
+                patterns.Add(new List<string>(pattern));
+            }
+
+            int sum = 0;
+
+            foreach(var p in patterns)
+            {
+                int colCountLeftOfSplit = 0;
+                int colCountAboveSplit = 0;
+                bool verticalSplit = false;
+
+                for(int i = 0; i < p.ElementAt(0).Length - 1; i++)
+                {
+                    string col1 = String.Join(String.Empty, p.Select(x => x[i]).ToList());
+                    string col2 = String.Join(String.Empty, p.Select(x => x[i + 1]).ToList());
+
+                    if (col1 == col2)
+                    {
+                        if(ReflectsOutwards(Orientation.Vertical, p, i, 1))
+                        {
+                            colCountLeftOfSplit = i;
+                            sum += (i + 1);
+                            verticalSplit = true;
+                        }
+                    }
+                }
+
+
+                if (!verticalSplit)
+                {
+                    for (int i = 0; i < p.Count - 1; i++)
+                    {
+                        string row1 = p[i];
+                        string row2 = p[i + 1];
+
+                        if (row1 == row2)
+                        {
+                            if (ReflectsOutwards(Orientation.Horizontal, p, i, 1))
+                            {
+                                sum += ((i + 1) * 100);
+                                colCountAboveSplit = i;
+                            }
+                        }
+                    }
+                }
+
+            }
+
+
+            return sum;
         }
 
         public int Problem2()
         {
-            string? line;
-            using (var fileStream = File.OpenRead(InputFile))
-            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true))
-            {
-                while ((line = streamReader.ReadLine()) != null)
-                {
+            string[] lines = File.ReadAllLines(InputFile);
+            List<List<string>> patterns = new List<List<string>>();
+            List<string> pattern = new List<string>();
+            List<Tuple<Orientation, int>> patternOriginalAnswers = new List<Tuple<Orientation, int>>();
 
+            for (int lineNum = 0; lineNum < lines.Length; lineNum++)
+            {
+                if (String.IsNullOrEmpty(lines[lineNum]))
+                {
+                    patterns.Add(new List<string>(pattern));
+                    pattern.Clear();
+                    continue;
+                }
+
+                pattern.Add(lines[lineNum]);
+            }
+
+            if (pattern.Count > 0)
+            {
+                patterns.Add(new List<string>(pattern));
+            }
+
+            
+
+            // Part 1 answers
+            foreach (var p in patterns)
+            {
+                bool verticalSplit = false;
+
+                for (int i = 0; i < p.ElementAt(0).Length - 1; i++)
+                {
+                    string col1 = String.Join(String.Empty, p.Select(x => x[i]).ToList());
+                    string col2 = String.Join(String.Empty, p.Select(x => x[i + 1]).ToList());
+
+                    if (col1 == col2)
+                    {
+                        if (ReflectsOutwards(Orientation.Vertical, p, i, 1))
+                        {
+                            patternOriginalAnswers.Add(new Tuple<Orientation, int>(Orientation.Vertical, i));
+                            verticalSplit = true;
+                        }
+                    }
+                }
+
+
+                if (!verticalSplit)
+                {
+                    for (int i = 0; i < p.Count - 1; i++)
+                    {
+                        string row1 = p[i];
+                        string row2 = p[i + 1];
+
+                        if (row1 == row2)
+                        {
+                            if (ReflectsOutwards(Orientation.Horizontal, p, i, 1))
+                            {
+                                patternOriginalAnswers.Add(new Tuple<Orientation, int>(Orientation.Horizontal, i));
+                            }
+                        }
+                    }
                 }
             }
 
-            return 0;
+            int patternIndex = 0;
+            int sum = 0;
+
+            foreach (var p in patterns)
+            {
+                Tuple<Orientation, int> patternOriginalSolution = patternOriginalAnswers.ElementAt(patternIndex);
+                bool reflectionFound = false;
+
+                for (int i = 0; i < p.ElementAt(0).Length - 1; i++)
+                {
+                    if (reflectionFound)
+                    {
+                        break;
+                    }
+
+                    string col1 = String.Join(String.Empty, p.Select(x => x[i]).ToList());
+                    string col2 = String.Join(String.Empty, p.Select(x => x[i + 1]).ToList());
+
+                    // These two cols are one char apart (the blur), try a normal reclection on other rows
+                    if(OffByOne(col1, col2) != -1)
+                    {
+                        if (ReflectsOutwardsWithBlur(Orientation.Vertical, p, i, 1, true))
+                        {
+                            sum += (i + 1);
+                            reflectionFound = true;
+                        }
+                    }
+
+                    // Cols identical, can't reflect here again
+                    if (col1 == col2 &&
+                        !(patternOriginalSolution.Item1 == Orientation.Vertical && patternOriginalSolution.Item2 == i))
+                    {
+                        if (ReflectsOutwardsWithBlur(Orientation.Vertical, p, i, 1, false))
+                        {
+                            sum += (i + 1);
+                            reflectionFound = true;
+                        }
+                    }
+                }
+
+
+                if (!reflectionFound)
+                {
+                    for (int i = 0; i < p.Count - 1; i++)
+                    {
+                        if (reflectionFound)
+                        {
+                            break;
+                        }
+
+                        string row1 = p[i];
+                        string row2 = p[i + 1];
+
+                        // These two cols are one char apart (the blur), try a normal reclection on other rows
+                        if (OffByOne(row1, row2) != -1)
+                        {
+                            if (ReflectsOutwardsWithBlur(Orientation.Horizontal, p, i, 1, true))
+                            {
+                                sum += ((i + 1) * 100);
+                                reflectionFound = true;
+                            }
+                        }
+
+                        // Cols identical, can't reflect here again
+                        if (row1 == row2 &&
+                            !(patternOriginalSolution.Item1 == Orientation.Horizontal && patternOriginalSolution.Item2 == i))
+                        {
+                            if (ReflectsOutwardsWithBlur(Orientation.Horizontal, p, i, 1, false))
+                            {
+                                sum += ((i + 1) * 100);
+                                reflectionFound = true;
+                            }
+                        }
+                    }
+                }
+
+                patternIndex++;
+            }
+
+            return sum;
+        }
+
+
+        private bool ReflectsOutwards(Orientation orientation, List<string> pattern, int index, int offset)
+        {
+
+            string segment1 = String.Empty;
+            string segment2 = String.Empty;
+
+            if (orientation == Orientation.Vertical)
+            {
+                if (index - offset < 0 || index + offset + 1 >= pattern.ElementAt(0).Length)
+                {
+                    return true;
+                }
+
+                segment1 = String.Join(String.Empty, pattern.Select(x => x[index - offset]).ToList());
+                segment2 = String.Join(String.Empty, pattern.Select(x => x[index + offset + 1]).ToList());
+            }
+            else
+            {
+                if (index - offset < 0 || index + offset + 1 >= pattern.Count)
+                {
+                    return true;
+                }
+
+                segment1 = pattern.ElementAt(index - offset);
+                segment2 = pattern.ElementAt(index + offset + 1);
+            }
+
+            if(segment1 == segment2)
+            {
+                return ReflectsOutwards(orientation, pattern, index, ++offset);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool ReflectsOutwardsWithBlur(Orientation orientation, List<string> pattern, int index, int offset, bool blurSet)
+        {
+
+            string segment1 = String.Empty;
+            string segment2 = String.Empty;
+
+            if (orientation == Orientation.Vertical)
+            {
+                if (index - offset < 0 || index + offset + 1 >= pattern.ElementAt(0).Length)
+                {
+                    return true;
+                }
+
+                segment1 = String.Join(String.Empty, pattern.Select(x => x[index - offset]).ToList());
+                segment2 = String.Join(String.Empty, pattern.Select(x => x[index + offset + 1]).ToList());
+            }
+            else
+            {
+                if (index - offset < 0 || index + offset + 1 >= pattern.Count)
+                {
+                    return true;
+                }
+
+                segment1 = pattern.ElementAt(index - offset);
+                segment2 = pattern.ElementAt(index + offset + 1);
+            }
+
+            if (segment1 == segment2)
+            {
+                return ReflectsOutwardsWithBlur(orientation, pattern, index, ++offset, blurSet);
+            }
+            else if(OffByOne(segment1, segment2) != -1 && !blurSet)
+            {
+                return ReflectsOutwardsWithBlur(orientation, pattern, index, ++offset, true);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private int OffByOne(string a, string b)
+        {
+            int diffCount = 0;
+            int diffIndex = 0;
+            if(a.Length != b.Length)
+            {
+                return -1;
+            }
+
+            for(int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != b[i])
+                {
+                    diffIndex = i;
+                    diffCount++;
+                }
+            }
+
+            if(diffCount == 1)
+            {
+                return diffIndex;
+            }
+
+            return -1;
+        }
+
+        private enum Orientation
+        {
+            None,
+            Horizontal,
+            Vertical
         }
     }
 }
