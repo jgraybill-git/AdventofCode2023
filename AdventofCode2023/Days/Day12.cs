322d7d20a6160693ae68617d8785ca6fe9dfa747
Jacob Graybill <jgraybill@gatech.edu>
2023-12-17 15:57:49 -0500

    Day 16 Progress

--- AdventofCode2023/Days/Day12.cs
+++ AdventofCode2023/Days/Day12.cs
@@ -116,6 +116,191 @@ namespace AdventofCode2023.Days
             using (var fileStream = File.OpenRead(InputFile))
             using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true))
             {
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    string[] lineParts = line.Split(' ');
+                    char[] springRow = lineParts[0].ToCharArray();
+                    string springrRowStr = String.Empty;
+                    int[] sequenceConstraints = lineParts[1].Split(',').Select(x => Convert.ToInt32(x)).ToArray();
+                    List<char> charOpts = new List<char>() { '#', '.' };
+                    char placeHolder = '?';
+
+                    //springrRowStr = string.Concat(Enumerable.Repeat(new string(springRow) + "?", 5));
+                    //springrRowStr = springrRowStr.Remove(springrRowStr.Length - 1);
+                    ///springRow = springrRowStr.ToCharArray();
+
+                    //sequenceConstraints = string.Join(',', Enumerable.Repeat(lineParts[1], 5)).Split(',').Select(x => Convert.ToInt32(x)).ToArray();
+
+                    // Binary search tree with pruning
+
+                    List<TreeNode> tree = new List<TreeNode>();
+
+                    TreeNode rootNode = new TreeNode(springRow.ElementAt(0), null);
+
+                    if (springRow[0] == '?')
+                    {
+                        springRow[0] = '.';
+                        rootNode.Value = '.';
+                        ConstructTree(rootNode, springRow, sequenceConstraints, 1, ref tree);
+
+                        springRow[0] = '#';
+                        rootNode.Value = '#';
+                        ConstructTree(rootNode, springRow, sequenceConstraints, 1, ref tree);
+                    }
+                    else
+                    {
+                        ConstructTree(rootNode, springRow, sequenceConstraints, 1, ref tree);
+                    }
+
+                    List<TreeNode> fd = tree.Where(x => x.RulesSatisfiedCount == sequenceConstraints.Length - 1).ToList();
+                    Console.WriteLine(fd.Count);
+                    
+
+                }
+            }
+
+            return 0;
+        }
+
+        private void ConstructTree(TreeNode parentNode, char[] row, int[] rules, int layer, ref List<TreeNode> tree)
+        {
+            TreeNode leftNode = null;
+            TreeNode rightNode = null;
+
+            if (row[layer] == '?')
+            {
+                leftNode = new TreeNode('.', parentNode);
+                leftNode.RulesSatisfiedCount = parentNode.RulesSatisfiedCount;
+                rightNode = new TreeNode('#', parentNode);
+                rightNode.RulesSatisfiedCount = parentNode.RulesSatisfiedCount;
+            }
+            else
+            {
+                // # or .   No need to branch a non-variable into two paths
+                leftNode = new TreeNode(row[layer], parentNode);
+                leftNode.RulesSatisfiedCount = parentNode.RulesSatisfiedCount;
+            }
+
+
+            for(int i = 0; i < 2; i++)
+            {
+                TreeNode? node = (i == 0) ? leftNode : rightNode;
+                
+                if (node == null)
+                {
+                    break;
+                }
+
+                string reverseSequence = Convert.ToString(node.Value);
+               
+
+                TreeNode parent = node.ParentNode;
+                while (parent != null)
+                {
+                    reverseSequence += parent.Value;
+                    parent = parent.ParentNode;
+                }
+
+                string sequence = String.Empty;
+                
+
+                for(int j = reverseSequence.Length - 1; j >= 0; j--)
+                {
+                    sequence += reverseSequence[j];
+                }
+
+                if(sequence == "..#..")
+                {
+                    int y = 6;
+                }
+                
+                if (ValidToRule(sequence, rules, node.RulesSatisfiedCount) || sequence.All(x => x == '.'))
+                {
+                    Console.WriteLine(sequence + " " + node.RulesSatisfiedCount);
+                    if (!sequence.All(x => x == '.'))
+                    {
+                        node.RulesSatisfiedCount++;
+                    }
+                    
+                    tree.Add(node);
+                    if(layer + 1 < row.Length)
+                    {
+                        ConstructTree(node, row, rules, ++layer, ref tree);
+                    }
+                    
+                }
+            }
+            
+
+        }
+
+
+        private bool ValidToRule(string sequence, int[] rules, int ruleNumber)
+        {
+            List<int> groupings = new List<int>();
+            int groupLen = 0;
+
+            if(sequence.Length >=3 && sequence.Substring(0,3) == "..#")
+            {
+                int g = 34;
+            }
+            for(int i = 0; i < sequence.Length; i++)
+            {
+                if (sequence[i] == '#')
+                {
+                    groupLen++;
+                }
+                else if(groupLen > 0)
+                {
+                    groupings.Add(groupLen);
+                    groupLen = 0;
+                }
+                else
+                {
+                    groupLen = 0;
+                }
+            }
+
+            if(groupLen > 0)
+            {
+                groupings.Add(groupLen);
+            }
+
+            for(int i = 0; i < ruleNumber + 1; i++)
+            {
+                if (!(i < groupings.Count && rules[i] == groupings[i]))
+                {
+                    return false;
+                }
+                
+            }
+
+            return true;
+        }
+
+        private bool RulesSatisfied(string sequence, int[] rules)
+        {
+            string rule = "(^|[^#]+)";
+
+            foreach (int matchCount in rules)
+            {
+                rule += $"#{{{matchCount}}}[^#]+";
+            }
+            rule = rule.Remove(rule.Length - 5) + "($|(?![^#]*#))";
+
+            Regex constraint = new Regex(rule);
+            Match match = constraint.Match(sequence);
+
+            return match.Success && match.Value.Count(x => x == '#') == sequence.Count(x => x == '#');
+        }
+
+        public int Problem2Old()
+        {
+            string? line;
+            int solutionCount = 0;
+            using (var fileStream = File.OpenRead(InputFile))
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true))
+            {
                 int lineNum = 1;
                 while ((line = streamReader.ReadLine()) != null)
                 {
@@ -180,6 +365,22 @@ namespace AdventofCode2023.Days
             }
 
             return solutionCount;
+        }
+
+        
+        private class TreeNode
+        {
+            public TreeNode? ParentNode { get; set; }
+            public char Value { get; set; }
+            public int RulesSatisfiedCount { get; set; }
+
+            public TreeNode(char val, TreeNode? node)
+            {
+                ParentNode = node;
+                Value = val;
+                RulesSatisfiedCount = 0;
+            }
+
         }
 
         private char[] RemoveInsufficientUnknowns(char[] springRow, int[] sequenceConstraints)
--- AdventofCode2023/Days/Day16.cs
+++ AdventofCode2023/Days/Day16.cs
@@ -20,18 +20,142 @@ namespace AdventofCode2023.Days
 
         public int Problem1()
         {
+            List<List<char>> inputGrid = new List<List<char>>();
+
+
             string? line;
             using (var fileStream = File.OpenRead(InputFile))
             using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true))
             {
                 while ((line = streamReader.ReadLine()) != null)
                 {
-
+                    inputGrid.Add(line.ToCharArray().ToList());
                 }
             }
 
-            return 0;
+            char[,] grid = new char[inputGrid.Count, inputGrid.ElementAt(0).Count];
+
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    grid[i, j] = inputGrid.ElementAt(i).ElementAt(j);
+                }
+            }
+
+            inputGrid.Clear();
+
+            char startChar = grid[0, 0];
+            Direction startDirection = Direction.Right;
+
+            GridTile startTile = new GridTile(0, 0, startDirection);
+
+            List<GridTile> energizedTiles = new List<GridTile>()
+            {
+                new GridTile(startTile.X, startTile.Y, startTile.Direction)
+            };
+
+            List<GridTile> activePaths = new List<GridTile>()
+            {
+                new GridTile(startTile.X, startTile.Y, startTile.Direction)
+            };
+
+
+            while (!activePaths.All(x => x.Terminate))
+            {
+                List<GridTile> newTiles = new List<GridTile>();
+                List<GridTile> removeTiles = new List<GridTile>();
+
+                foreach (GridTile currentTile in activePaths)
+                {
+                    if (!currentTile.Terminate)
+                    {
+                        List<Direction> goInDirections = AnalyseTile(currentTile,
+                            grid[currentTile.X, currentTile.Y]);
+
+                        if (goInDirections.Count == 1)
+                        {
+                            currentTile.Direction = goInDirections.First();
+                            GridTile moveResult = MoveToNextTile(currentTile, ref grid);
+
+                            if (!moveResult.Terminate || !BeenToTileViaDirection(moveResult, ref energizedTiles))
+                            {
+                                currentTile.X = moveResult.X;
+                                currentTile.Y = moveResult.Y;
+
+                                energizedTiles.Add(new GridTile(moveResult.X, moveResult.Y, moveResult.Direction));
+                            }
+                            else
+                            {
+                                currentTile.Terminate = true;
+                                removeTiles.Add(currentTile);
+                            }
+                        }
+                        else
+                        {
+                            currentTile.Direction = goInDirections[0];
+                            GridTile moveResultOne = MoveToNextTile(currentTile, ref grid);
+
+                            currentTile.Direction = goInDirections[1];
+                            GridTile moveResultTwo = MoveToNextTile(currentTile, ref grid);
+
+                            if (!moveResultOne.Terminate && !BeenToTileViaDirection(moveResultOne, ref energizedTiles))
+                            {
+                                newTiles.Add(moveResultOne);
+                                energizedTiles.Add(new GridTile(moveResultOne.X, moveResultOne.Y, moveResultOne.Direction));
+                            }
+                            if (!moveResultTwo.Terminate && !BeenToTileViaDirection(moveResultTwo, ref energizedTiles))
+                            {
+                                newTiles.Add(moveResultTwo);
+                                energizedTiles.Add(new GridTile(moveResultTwo.X, moveResultTwo.Y, moveResultTwo.Direction));
+                            }
+
+                            if (newTiles.Count == 0 || newTiles.Count == 2)
+                            {
+                                currentTile.Terminate = true;
+                                removeTiles.Add(currentTile);
+                            }
+                            else if(newTiles.Count == 1)
+                            {
+                                newTiles.Add(new GridTile(newTiles.First().X, newTiles.First().Y, newTiles.First().Direction));
+                                removeTiles.Add(currentTile);
+                            }
+                        }
+                    }
+                }
+
+                foreach(var tile in removeTiles)
+                {
+                    activePaths.Remove(tile);
+                }
+                foreach (var tile in newTiles)
+                {
+                    activePaths.Add(tile);
+                }
+            }
+
+            var distinctTiles = GetDistinctTiles(energizedTiles);
+
+            for (int x = 0; x < grid.GetLength(0); x++)
+            {
+                Console.WriteLine();
+                for (int y = 0; y < grid.GetLength(1); y++)
+                {
+                    if (distinctTiles.Where(z => z.X == x && z.Y == y).Any())
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+            }
+            Console.WriteLine();
+
+            return distinctTiles.Count;
         }
+
 
         public int Problem2()
         {
@@ -46,6 +170,191 @@ namespace AdventofCode2023.Days
             }
 
             return 0;
+        }
+
+        private bool BeenToTileViaDirection(GridTile moveResult, ref List<GridTile> energizedTiles)
+        {
+            foreach(var tile in energizedTiles)
+            {
+                if(tile.X == moveResult.X && tile.Y == moveResult.Y && tile.Direction == moveResult.Direction)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private List<GridTile> GetDistinctTiles(List<GridTile> tiles)
+        {
+            List<GridTile> visitedTiles = new List<GridTile>();
+
+            foreach (var tile in tiles)
+            {
+                if (!ListContains(visitedTiles, tile))
+                {
+                    visitedTiles.Add(tile);
+                }
+            }
+
+            return visitedTiles;
+        }
+
+        private bool ListContains(List<GridTile> squares, GridTile square)
+        {
+            foreach(var s in squares)
+            {
+                if (s.X == square.X && s.Y == square.Y)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private List<Direction> AnalyseTile(GridTile tile, char squareItem)
+        {
+            switch (tile.Direction)
+            {
+                case Direction.Up:
+                    switch (squareItem)
+                    {
+                        case '.':
+                            return new List<Direction>() { tile.Direction};
+                        case '-':
+                            return new List<Direction>()
+                            {
+                                Direction.Left,
+                                Direction.Right
+                            };
+                        case '|':
+                            return new List<Direction>() { tile.Direction };
+                        case '/':
+                            return new List<Direction>() { Direction.Right };
+                        case '\\':
+                            return new List<Direction>() { Direction.Left };
+
+                    }
+                    break;
+                case Direction.Down:
+                    switch (squareItem)
+                    {
+                        case '.':
+                            return new List<Direction>() { tile.Direction };
+                        case '-':
+                            return new List<Direction>()
+                            {
+                                Direction.Left,
+                                Direction.Right
+                            };
+                        case '|':
+                            return new List<Direction>() { tile.Direction };
+                        case '/':
+                            return new List<Direction>() { Direction.Left };
+                        case '\\':
+                            return new List<Direction>() { Direction.Right };
+
+                    }
+                    break;
+                case Direction.Left:
+                    switch (squareItem)
+                    {
+                        case '.':
+                            return new List<Direction>() { tile.Direction };
+                        case '-':
+                            return new List<Direction>() { tile.Direction };
+                        case '|':
+                            return new List<Direction>()
+                            {
+                                Direction.Up,
+                                Direction.Down
+                            };
+                        case '/':
+                            return new List<Direction>() { Direction.Down };
+                        case '\\':
+                            return new List<Direction>() { Direction.Up };
+
+                    }
+                    break;
+                case Direction.Right:
+                    switch (squareItem)
+                    {
+                        case '.':
+                            return new List<Direction>() { tile.Direction };
+                        case '-':
+                            return new List<Direction>() { tile.Direction };
+                        case '|':
+                            return new List<Direction>()
+                            {
+                                Direction.Up,
+                                Direction.Down
+                            };
+                        case '/':
+                            return new List<Direction>() { Direction.Up };
+                        case '\\':
+                            return new List<Direction>() { Direction.Down };
+
+                    }
+                    break;
+            }
+
+            return new List<Direction>() { Direction.Right };
+        }
+
+        private GridTile MoveToNextTile(GridTile current, ref char[,] grid)
+        {
+            int rightMove = current.Y + 1;
+            int leftMove = current.Y - 1;
+            int upMove = current.X - 1;
+            int downMove = current.X + 1;
+
+            if (current.Direction == Direction.Up && upMove >= 0)
+            {
+                return new GridTile(upMove, current.Y, current.Direction);
+            }
+            else if (current.Direction == Direction.Down && downMove < grid.GetLength(0))
+            {
+                return new GridTile(downMove, current.Y, current.Direction);
+            }
+            else if (current.Direction == Direction.Left && leftMove >= 0)
+            {
+                return new GridTile(current.X, leftMove, current.Direction);
+            }
+            else if (current.Direction == Direction.Right && rightMove < grid.GetLength(1))
+            {
+                return new GridTile(current.X, rightMove, current.Direction);
+            }
+            else
+            {
+                return new GridTile(current.X, current.Y, current.Direction, true);
+            }
+        }
+
+
+
+        private class GridTile
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public Direction Direction { get; set; }
+            public bool Terminate { get; set; }
+            
+            public GridTile(int x, int y, Direction d, bool t = false)
+            {
+                X = x;
+                Y = y;
+                Direction = d;
+                Terminate = t;
+            }
+        }
+
+        private enum Direction
+        {
+            Up,
+            Down,
+            Left,
+            Right
         }
     }
 }
